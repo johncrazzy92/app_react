3e849408aaa8de49c5e96bf6b7d8a6c5635c237b
juan pablo romero <jpr.1992@hotmail.com>
2023-08-28 04:21:59 -0300

    composition

---
+++ src/layouts/LayoutHome.jsx
@@ -0,0 +1,16 @@
+
+import NavBar from './NavBar'
+import background from "../../public/img/Background.png"
+import Welcome from '../components/Welcome'
+
+
+const LayoutHome = () => {
+  return (
+    <div className='w-screen bg-no-repeat bg-cover flex flex-col p-5 gap-16 h-screen lg:h-2/3 lg:px-16 lg:py-6' style={{backgroundImage:`url(${background})`}}>
+    <NavBar />
+    <Welcome />
+    </div>
+  )
+}
+
+export default LayoutHome
