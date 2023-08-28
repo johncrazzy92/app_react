b5b11351215642da1aa5a6306fcd2267d9be9c99
juan pablo romero <jpr.1992@hotmail.com>
2023-08-28 18:03:39 -0300

    correcciones

--- src/layouts/LayoutHome.jsx
+++ src/layouts/LayoutHome.jsx
@@ -6,7 +6,7 @@ import Welcome from '../components/Welcome'
 
 const LayoutHome = () => {
   return (
-    <div className='w-screen bg-no-repeat bg-cover flex flex-col p-5 gap-16 h-screen lg:h-2/3 lg:px-16 lg:py-6' style={{backgroundImage:`url(${background})`}}>
+    <div className='w-full bg-cover flex flex-col p-5 gap-16 h-screen lg:h-2/3 lg:px-16 lg:py-6' style={{backgroundImage:`url(${background})`}}>
     <NavBar />
     <Welcome />
     </div>
