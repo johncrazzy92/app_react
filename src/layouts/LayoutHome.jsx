c6e30106ea1f7d1396f5cf6ab8be6c48e329df0e
juan pablo romero <jpr.1992@hotmail.com>
2023-08-29 01:43:36 -0300

    layout

--- src/layouts/LayoutHome.jsx
+++ src/layouts/LayoutHome.jsx
@@ -2,14 +2,18 @@
 import NavBar from './NavBar'
 import background from "../../public/img/Background.png"
 import Welcome from '../components/Welcome'
+import Carousel from '../components/Carousel'
 
-
-const LayoutHome = () => {
+const LayoutHome = (props) => {
+console.log(props.children);
   return (
-    <div className='w-full bg-cover flex flex-col p-5 gap-16 h-screen lg:h-2/3 lg:px-16 lg:py-6' style={{backgroundImage:`url(${background})`}}>
-    <NavBar />
-    <Welcome />
-    </div>
+    <>
+      <div className='w-full bg-cover flex flex-col p-5 gap-16 h-screen lg:h-2/3 lg:px-16 lg:py-6' style={{backgroundImage:`url(${background})`}}>
+        <NavBar />
+        <Welcome />
+      </div>
+      <Carousel />
+    </>
   )
 }
 
