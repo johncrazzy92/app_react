3cd142d3c2e81628d2c241efe23fc95f126abf55
juan pablo romero <jpr.1992@hotmail.com>
2023-08-30 02:58:33 -0300

    componentes varios

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,4 @@
+import Button from "./Button"
 
 
 const Welcome = () => {
@@ -6,7 +7,7 @@ const Welcome = () => {
           <h1 className='sm:text-left text-white text-4xl lg:text-6xl font-bold mb-1'>For the love of manga</h1>
           <p className='sm:text-left text-white text-2xl'>Explore our varieties</p>
           <p className='sm:text-left text-white '>#Mingalove❤️ </p>
-          <button className='text-orange-600 w-full sm:w-60 bg-white border p-3.5 gap-2.5 rounded-md text-2xl flex justify-center items-center pt-2'>Sign In!</button>
+          <Button/>
     </div>
   )
 }
