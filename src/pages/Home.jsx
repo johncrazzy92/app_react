10ddde8b773983487170a3a3f3e353f77009c6e5
juan pablo romero <jpr.1992@hotmail.com>
2023-08-30 16:46:50 -0300

    1.01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import router from '../router/router'
 
-import LayoutHome from '../layouts/LayoutHome'
-import Footer from '../components/Footer'
 
 
 const Home = () => {
