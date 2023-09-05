4d079ef9575c0eed57a67a6a9bc80732d7790bc1
juan pablo romero <jpr.1992@hotmail.com>
2023-09-05 11:56:35 -0300

    version arreglada

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,16 @@
-import Button from "./Button"
-
+import Button from "./Button";
 
 const Welcome = () => {
   return (
-    <div className='flex h-full flex-col px-6 lg:px-16 lg:py-6 gap-1 items-center justify-center lg:items-start lg:justify-start'>
-          <h1 className='sm:text-left text-white text-4xl lg:text-6xl font-bold mb-1'>For the love of manga</h1>
-          <p className='sm:text-left text-white text-2xl'>Explore our varieties</p>
-          <p className='sm:text-left text-white '>#Mingalove❤️ </p>
-          <Button/>
+    <div className="flex h-full flex-col px-6 lg:px-16 lg:py-6 gap-1 items-center justify-center lg:items-start lg:justify-start">
+      <h1 className="sm:text-left text-white text-4xl lg:text-6xl font-bold mb-1 text-center">
+        For the love of manga
+      </h1>
+      <p className="sm:text-left text-white text-2xl">Explore our varieties</p>
+      <p className="sm:text-left hidden lg:block text-white ">#Mingalove❤️ </p>
+      <Button />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,27 +1,33 @@
-import  menuhamburguesa  from "../../public/img/Menu.png";
-import  logomenu  from "../../public/img/Logo Dos.png";
+import menuhamburguesa from "../../public/img/Menu.png";
+
 import { useState } from "react";
 import Display from "../components/Display";
 
-
 function NavBar() {
-  const [open, setOpen] = useState(false)
-  const displayMenu = ()=>{
-    setOpen(!open)
-  }
+  const [open, setOpen] = useState(false);
+  const displayMenu = () => {
+    setOpen(!open);
+  };
   return (
-
     <>
-    <nav className='w-100 flex m-5 justify-between lg:m-10 lg:flex'>
-        <img src={menuhamburguesa} alt="hamburguesa" onClick={displayMenu} className="cursor-pointer" />
+      <nav className="w-100 flex m-5 justify-between lg:m-10 lg:flex">
+        <img
+          src={menuhamburguesa}
+          alt="hamburguesa"
+          onClick={displayMenu}
+          className="cursor-pointer"
+        />
+        <div className="flex">
+          <p className="lg:flex items-center hidden text-orange-600 text-4xl">
+            Minga
+          </p>
+          <p className="flex items-center  text-white text-5xl">雪</p>
+        </div>
+      </nav>
 
-        <img src={logomenu} alt="Logo minga" />
-    </nav>
-      
-       <Display close = {open} switchMenu ={displayMenu}/>
-      
+      <Display close={open} switchMenu={displayMenu} />
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
